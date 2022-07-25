2d59cda14bd1e90e7ee54c9ed57a1f08d411e953
Sahejpalarneja <sahejpalarneja@gmail.com>
2022-07-25 23:31:27 +0200

    Debug on

--- Study_Group_2/settings.py
+++ Study_Group_2/settings.py
@@ -26,7 +26,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-m0t372j@q2we)f_!mf-b&caes^0=c#rc9gw7-85h=u5t^^kkpx'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ['https://study-group-2.herokuapp.com/']
 
