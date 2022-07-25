3bfc8a07e9cf3d1f81964024ea190b04780c56c1
Sahejpalarneja <sahejpalarneja@gmail.com>
2022-07-25 22:38:36 +0200

    Added static

--- Study_Group_2/settings.py
+++ Study_Group_2/settings.py
@@ -27,7 +27,7 @@ SECRET_KEY = 'django-insecure-m0t372j@q2we)f_!mf-b&caes^0=c#rc9gw7-85h=u5t^^kkpx
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = ['masterminds.com']
+ALLOWED_HOSTS = ['https://study-group-2.herokuapp.com/']
 
 
 # Application definition
