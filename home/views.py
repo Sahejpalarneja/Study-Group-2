33d984d9ae2e5b177442fdad977bead48713dd0d
Sahejpalarneja <sahejpalarneja@gmail.com>
2022-08-03 23:02:08 +0200

    Added registration templates, urls and clickables

--- home/views.py
+++ home/views.py
@@ -5,3 +5,9 @@ from django.shortcuts import render
 
 def home_page_view(request):
     return render(request,'home.html')
+
+def login_page(request):
+    return render(request,'registration/login.html')
+
+def register_page(request):
+    return render(request,'registration/register.html')
