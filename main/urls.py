b465bac559e01e1e7df04aaa2b22f05404d1f061
Sahejpalarneja <sahejpalarneja@gmail.com>
2022-08-18 12:27:34 +0200

    Updates, view, added url,created form

--- main/urls.py
+++ main/urls.py
@@ -3,5 +3,6 @@ from . import views
 app_name = 'main'
 urlpatterns = [
     #path('',views.MainPageView.as_view(),name='main'),
-    path('',views.MainPageView.as_view(),name='main')
+    path('',views.MainPageView.as_view(),name='main'),
+    path('add/',views.SubjectCreate.as_view(),name='add')
 ]
--- main/views.py
+++ main/views.py
@@ -1,9 +1,25 @@
+from dataclasses import field
+from re import template
 from django.shortcuts import render
+from .forms import SubjectModelForm
+from django.views.generic.edit import CreateView, UpdateView, DeleteView
 from django.contrib.auth.mixins import LoginRequiredMixin
+from django.urls import reverse_lazy
+from bootstrap_modal_forms.generic import BSModalCreateView
+
+
 from django.views import View
+from .models import Subject
 # Create your views here.
 class MainPageView(LoginRequiredMixin,View):
     login_url = 'login'
     def get(self,request):
         ctx = {'user':request.user}
         return render(request,'main/main.html',ctx)
+
+
+class SubjectCreate(LoginRequiredMixin,BSModalCreateView):
+    template_name = 'main/subject_form.html'
+    form_class = SubjectModelForm
+    success_message = 'Success: Subject was added'
+    success_url = reverse_lazy('autos:all')
