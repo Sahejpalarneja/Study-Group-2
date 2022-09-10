b49b887d785b759eb86abb2afe51299d5c2186e6
Sahejpalarneja <sahejpalarneja@gmail.com>
2022-09-10 13:23:02 +0200

    added get message view

--- main/views.py
+++ main/views.py
@@ -1,12 +1,14 @@
 
-
+from datetime import datetime
+from django.http import HttpResponse
 from django.contrib import messages
 from django.shortcuts import render
 from .forms import SubjectModelForm
 from django.contrib.auth.mixins import LoginRequiredMixin
 from django.urls import reverse_lazy
 from bootstrap_modal_forms.generic import BSModalCreateView
-from django.http import HttpResponseRedirect,response
+import json
+from django.http import HttpResponseRedirect
 
 
 from django.views import View
@@ -18,15 +20,7 @@ class MainPageView(LoginRequiredMixin,View):
         student = request.user
         subject_ids = SubjectStudent.objects.filter(student_id = student.id).only('subject_id')
         subjects = [Subject.objects.get(neptun = id)for id in subject_ids]
-        messages ={}
-        for subject in subjects:
-            l = Message.objects.filter(N_code = subject.neptun).order_by('-timestamp')
-            if l is None:
-                messages[subject.name] = None
-            else:
-                message_list = [{'sender':message.sender,'text':message.text} for message in l ]
-                messages[subject.name] = message_list
-        ctx = {'user':student,'subjects':subjects,'messages':messages}
+        ctx = {'user':student,'subjects':subjects}
         return render(request,'main/main.html',ctx)
 
 class SubjectCreate(LoginRequiredMixin,BSModalCreateView):
@@ -73,6 +67,22 @@ class SubjectJoin(LoginRequiredMixin,View):
 
 def send_message(request):
     if request.method == 'POST':
-        print("This is working")
-        return response(status_code = 210)
-    return HTTPResponse("Dikkat")
+        sender = request.POST['sender']
+        message = request.POST['message']
+        timestamp = datetime.now()
+        subject = request.POST['subject']
+        n_code = Subject.objects.get(name = subject)
+        message_obj = Message(sender = sender,text = message,N_code = n_code.neptun ,timestamp = timestamp)
+        message_obj.save()
+        return HttpResponse('Success')
+    return HttpResponse("Dikkat")
+
+def get_message(request):
+    if request.method == 'GET':
+        subject = Subject.objects.get(name = request.GET['subject'] )
+        l = Message.objects.filter(N_code = subject.neptun).order_by('timestamp')
+        print(len(l))
+        message_list = [{'sender':message.sender,'text':message.text} for message in l ]
+        message_list = json.dumps(message_list,indent=2)
+        return HttpResponse(message_list)
+        
