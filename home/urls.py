cd692745a7d152d68771b00b63596c9859f48a28
Sahejpalarneja <sahejpalarneja@gmail.com>
2022-09-20 11:51:34 +0200

    Implemented logout

--- home/urls.py
+++ home/urls.py
@@ -5,5 +5,6 @@ from . import views
 urlpatterns = [
     path('',views.home_page,name='home'),
     path('register/',views.register_page,name='register'),
-    path('login/',views.login_page,name = 'login')
+    path('login/',views.login_page,name = 'login'),
+    path('logout/',views.logout,name = 'logout')
 ]
