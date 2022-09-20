60a32df6edafe76640f51b7e1f545952b23d49f7
Sahejpalarneja <sahejpalarneja@gmail.com>
2022-09-20 11:30:54 +0200

    small refactor

--- home/views.py
+++ home/views.py
@@ -45,6 +45,6 @@ def register_page(request):
 
 
 @receiver(post_save,sender = settings.AUTH_USER_MODEL)
-def crreate_auth_token(sender,instance = None,created= False, **kwargs):
+def create_auth_token(sender,instance = None,created= False, **kwargs):
 	if created:
 		Token.objects.create(user = instance)
