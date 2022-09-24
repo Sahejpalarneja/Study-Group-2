6569b2618335cf93de257dd3dc8fd8659191d489
Sahejpalarneja <sahejpalarneja@gmail.com>
2022-09-24 17:41:12 +0200

    Added user classes endpoint

--- api/urls.py
+++ api/urls.py
@@ -7,5 +7,6 @@ urlpatterns = [
     path('subjects',views.get_subjects,name = 'subjects'),
     path('register',views.regiter_user,name = 'register'),
     path('login',obtain_auth_token,name = 'login'),
-    path('get_id',views.get_user_id,name='get_id')
+    path('get_id',views.get_user_id,name='get_id'),
+    path('get_user_subjects',views.get_user_classes,name='user_classes')
 ]
